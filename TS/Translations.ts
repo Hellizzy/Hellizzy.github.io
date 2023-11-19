515fb87723be731778931c06e324c562da0a62a7
Hellizzy <jdawe@hotmail.ca>
2023-11-18 19:11:32 -0500

    .

--- TS/Translations.ts
+++
@@ -1,6 +0,0 @@
-import I18n from 'I18n';
-export const TranslationStrings = {
-  getButtonLabel: () => I18n.text('cmsf-branding.buttonLabel'),
-  getReportAbuseLinkText: () => I18n.text('cmsf-branding.dropdown.reportAbuse'),
-  getCmsPurchaseLinkText: () => I18n.text('cmsf-branding.dropdown.getCms')
-};
